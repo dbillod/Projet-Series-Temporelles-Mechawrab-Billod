03522a3a774c85bd5e5ec9696387345826fe8437
dbillod <david.billod.de.filiquier@ensae.fr>
2015-02-13 19:41:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 #include<strstream>
 #include<vector>
 #include<cmath>
-#include<typeinfo>
 #include<fstream>
 #include<sstream>
 #include<string>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 #include"Fonc.h"
 
@@ -28,8 +27,9 @@ int main()
     cout<<" "<<endl;
 
 
-      ///////////////////////////////////////////////////////////////////
-            //////////////// NOTA BENE ///////////////////
+     //////////////////////////////////////////////
+     //////////////// NOTA BENE ///////////////////
+     //////////////////////////////////////////////
 
     //Ce fichier doit avoir un présentation précise//
     //Il doit avoir à la première ligne une lettre//
@@ -46,35 +46,25 @@ int main()
     cout<<" "<<endl;
 	
 	cout<<"Veuillez indiquer le chemin d'acces du fichier txt contenant les series a utiliser"<<endl;
-    string chemin;
-    cin>>chemin;
+	cout<<"--------------------------------------------"<<endl;
+	cout<<"!!Le chemin ne doit pas contenir d'accents!!"<<endl;
+    cout<<"--------------------------------------------"<<endl;
 
+
+	string chemin;
+    getline(cin,chemin);
 
     cout<<"Combien de valeurs vous voulez prevoir (nombre entier) ?"<<endl;
     cout<<" "<<endl;
 
     int nbcoup;
     cin>> nbcoup;
-
-
-
-
-
   
+	string sflux = chemin;
 
-
-    //string sflux = "C:/Users/David/Documents/ENSAE/2A/C++/Series_PIB_Cons_Emploi.txt";
-    string sflux = "./seriesini.txt";
-    //string sflux ="./Series_PIB_Cons_Emploi.txt";
-	//string sflux = "W:/Bureau/C++/Projet C++/Prévision Séries Temp/seriesini.txt";
-	//string sflux = "//paradis/eleves/DBILLOD/Bureau/C++/Projet C++/Prévision Séries Temp/seriesini.txt";
-	//string sflux = chemin;
-
-	//cout<<"sflux = "<<sflux<<endl;
 
     ifstream monFlux(sflux);
-	//ifstream monFlux(" W:/Bureau/C++/Projet C++/Prévision Séries Temp/Prévision Séries Temp/seriesini.txt" );
-	//ifstream monFlux ("//paradis/eleves/DBILLOD/Bureau/C++/Projet C++/Prévision Séries Temp/Prévision Séries Temp/seriesini.txt");
+	
 
 
     if(monFlux)
@@ -83,8 +73,8 @@ int main()
 
     string ligne; //cette variable stocke les lignes lues
     vector <double> TAB;
-    int nbdouble = 0;
-    int nbseries = 0;
+    int nbdouble = 0; //cette variable stocke le nombre de valeurs totales du fichier
+    int nbseries = 0; //cette variable stocke le nombre de séries présentes dans le fichier
       while(getline(monFlux, ligne))
       {
          if (ligne=="fin")
@@ -108,19 +98,19 @@ int main()
       }
 
 
-        int tailletab = nbdouble/nbseries;
+        int tailletab = nbdouble/nbseries; //tailletab est la taille commune de toutes les séries
 
-        vector <vector <double> > tabdata = tabdata2(nbseries,tailletab,TAB);
+        vector <vector <double> > tabdata = construire_matrice_data(nbseries,tailletab,TAB); 
 
-////////////////////////////////////////////////////////////////
-    //On veut trouver les meilleurs k et h par cross validation
-////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    //On veut trouver les meilleurs k et h par cross validation//
+    /////////////////////////////////////////////////////////////
 
-        vector <double> erreurkh;
-        vector <double> erreurmoykh;
+        vector <double> erreurkh; //tableau qui stocke les erreurs de prédiction pour un couple (k,h) donné
+        vector <double> erreurmoykh; // tableau qui stocke les erreurs et le couple correspondant
 
-        vector <vector <double > > Indice_utileglobal;
-        vector <double>Ensemble_previsionsp0;
+        vector <vector <double > > Indice_utileglobal; //tableau de tableaux (matrice) dont le p-ème élément est le tableau des utlités des séries explicativesd pour la p-ème distance
+        vector <double>Ensemble_previsionsp0; //tableau qui stocke les prévisions pour la distance0, i.e distanceinf
         vector <double>Ensemble_previsionsp1;
         vector <double>Ensemble_previsionsp2;
 
@@ -135,16 +125,20 @@ int main()
             convert << i+1;
             Res = convert.str(); //on passe i en string
 
-            string sxi = "x" + Res;
+            string sxi = "x" + Res;//on crée xi
             Nom_series.push_back(sxi);
             }
 
+		  //On fixe le nombre maximum de plus proches voisins et la taille maximale des séquences à comparer
+		  int kmax=10; 
+		  int hmax=10;
 
-///////////////////////////////////////////////////////////////////////////
-//Pour prevoir plusieurs valeurs on va la faire distance par distance//////
-//Donc on crée 3 tableaux de tableaux qui stockeront les données updatees//
-///////////////////////////////////////////////////////////////////////////
-    vector <vector <double> >tabdatap0=tabdata;
+
+/////////////////////////////////////////////////////////////////////////////////////
+//Pour prevoir plusieurs valeurs on va le faire distance par distance////////////////
+//Donc on crée 3 tableaux de tableaux (matrice) qui stockeront les données updatées//
+/////////////////////////////////////////////////////////////////////////////////////
+    vector <vector <double> >tabdatap0=tabdata; //va stockerl les données updatées pour la distance0
     vector <vector <double> >tabdatap1=tabdata;
     vector <vector <double> >tabdatap2=tabdata;
 
@@ -163,6 +157,11 @@ int main()
             ////////////////////////////////////////////
             //On va faire varier le choix de distances//
             ////////////////////////////////////////////
+
+		  ////////////////////////////////////////////////////////////////////
+		  //Selon le choix de la distance, on ne prend pas les mêmes données//
+		  ////////////////////////////////////////////////////////////////////
+
             if(p==0){
                 tabdata = tabdatap0;
             }
@@ -174,20 +173,20 @@ int main()
             }
 
 
-      for(int k=1; k<10; k++){
+      for(int k=1; k<kmax; k++){
 
-	  for(int h=2; h<5;h++){
+	  for(int h=2; h<hmax;h++){
 
 
 
-	  for(int t = 5;  t< tailletab ;t++){
+	  for(int t = hmax;  t< tailletab ;t++){
 
-            vector < vector <double> > tabdata_coupe = coupertab(t, tabdata);
+            vector < vector <double> > tabdata_coupe = coupertab(t, tabdata); //matrice ou tableau de tableux qui va stocker les données tronquées
 
 		  //on va renormer les tableaux
 		vector <vector<double> >tabdata_r;
 		for(int i =0; i<nbseries;i++){
-			tabdata_r.push_back(renorm(tabdata_coupe[i]));
+			tabdata_r.push_back(renorm(tabdata_coupe[i])); //on renorme les données pour pouvoir les comparer
 		}
 
 
@@ -198,59 +197,42 @@ int main()
 
 
 
-				vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale toutes les sous équences de taille h
+				vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale toutes les sous équences de taille h de toutes les séries 
 
 				for(int i =0; i<nbseries;i++){
 				Mat_totale.push_back(matrice_sous_seq(tabdata_r[i],h));
 			   }
 
-				vector<double> Seq_a_predire = extract(tabdata_r[0],t-h,h);// Seq_a_predire est la dernière séq de y d etaille h
+				vector<double> Seq_a_predire = extract(tabdata_r[0],t-h,h);// Seq_a_predire est la dernière séquence de y de taille h
 
 
-                /*
-				for(int i =1; i<nbseries;i++){
-						vector <double> temp_disttab;
-						for (int indice_debut = 0; indice_debut<t-h+1;indice_debut++){
-							temp_disttab.push_back(  distance2( Mat_totale[i][indice_debut] , Seq_a_predire )  );
-						}
-						Tabdist.push_back( temp_disttab );
-						cout<<"i="<<i<<endl;
-				}*/
+                //Tabdist est un tableau de tableaux (matrice) dont le premier élément est un tableau de taille h qui comporte tailletab-h élémets qui sont les distances des séquences extraites par rapport à ce qu'il faut prédire
+                vector <vector <double>  >  Tabdist=construire_matrice_distance(nbseries,h,t,Mat_totale,Seq_a_predire,p);
 
 
-                //Tabdist est un tableau dont le premier élément est un tableau de taille h qui comprte tailletab-h élémets qui sont les distances des séquences extraites // à ce qu'il faut prédire
-                vector <vector <double>  >  Tabdist=tabdist(nbseries,h,t,Mat_totale,Seq_a_predire,p);
 
-
-				/*vector <double> Tabdist_simple ;
-				for (int i =0; i<nbseries-1;i++){
-					for (int j = 0;j<t-h+1;j++){
-					Tabdist_simple.push_back(Tabdist[i][j]);
-					}
-				}*/
-
-				vector <double> Tabdist_simple= simplifietab(nbseries, h,  t, Tabdist );
+				vector <double> Tabdist_simple= simplifietab(nbseries, h,  t, Tabdist ); 
 
 
 				vector <double> Positions_min = argminmultiple(Tabdist_simple,k);
 
 
 				vector < vector <double> > Seq_min; //seq-min contient les tableaux les plus proches de seq_a_predire
-				vector <double> Val_suiv; //Ce tableau va stocker les valeurs suivantes des seq_min
+				vector <double> Val_suiv; //Ce tableau va stocker les valeurs suivantes des seq_min, sur lesquelles on va moyenner pour obtenir notre prédiction
 
 				for(int i =0; i<k;i++){
 					int quotient = (Positions_min[i])/(t-h+1);
 					int reste = Positions_min[i]-quotient*(t-h+1);
 					Seq_min.push_back(Mat_totale[quotient+1][reste]);
 					if(reste == t-h){
-					 Val_suiv.push_back(Mat_totale[quotient+1][reste][h-1]);//Si on tombe sur le dernier terme d'une série, on ne prend pas la valeur suivante mais ce terme
+					 Val_suiv.push_back(Mat_totale[quotient+1][reste][h-1]);//Si on tombe sur le dernier terme d'une série, on ne prend pas la valeur suivante mais ce terme-là
 					}
 					else {
 					Val_suiv.push_back( Mat_totale[quotient+1][reste+1][h-1] );
 					}
 				}
 
-				double prevision = moyenne(Val_suiv);
+				double prevision = moyenne(Val_suiv); //Valeur prévue
 
 				erreurkh.push_back(abs(prevision-tabdata[0][t]));
 
@@ -267,11 +249,11 @@ int main()
 	  int longueur = erreurmoykh.size()/3;
 	  vector <double> erreurmoy;
 	  for (int i =1; i<longueur; i++ ){
-            erreurmoy.push_back(erreurmoykh[3*i-1]); // on stocke les m
+            erreurmoy.push_back(erreurmoykh[3*i-1]); // on stocke les m, i.e erreurs moyennes
 	  };
 
     int arg = argmin(erreurmoy);
-    int bestk = erreurmoykh[3*arg];
+    int bestk = erreurmoykh[3*arg]; //best k est notre k optimal par cross-validation
     int besth = erreurmoykh[3*arg +1];
 
 
@@ -319,8 +301,8 @@ vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale
 
 				vector<double> Seq_a_predire = extract(tabdata_r[0],tailletab-besth,besth);// Seq_a_predire est la dernière séq de y d etaille h
 
-                //Tabdist est un tableau dont le premier élément est un tableau de taille h qui comprte tailletab-h élémets qui sont les distances des séquences extraites // à ce qu'il faut prédire
-                vector <vector <double>  >  Tabdist=tabdist(nbseries,besth,tailletab,Mat_totale,Seq_a_predire,p);
+                //Tabdist est un tableau de tableaux (matrice) dont le premier élément est un tableau de taille h qui comprte tailletab-h élémets qui sont les distances des séquences extraites // à ce qu'il faut prédire
+                vector <vector <double>  >  Tabdist=construire_matrice_distance(nbseries,besth,tailletab,Mat_totale,Seq_a_predire,p);
 
 				vector <double> Tabdist_simple= simplifietab(nbseries, besth,  tailletab, Tabdist );
 
@@ -328,19 +310,18 @@ vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale
 
                 vector < vector <double> > Seq_min; //seq-min contient les tableaux les plus proches de seq_a_predire
 				vector <double> Val_suiv; //Ce tableau va stocker les valeurs suivantes des seq_min
-				vector <double> Indice_util;
+				vector <double> Indice_utile;
 				//double Indice_util[nbseries-1]; //On crée ce tableau pour savoir quelles séries ont été les plus utiles
 
 				for (int i=0; i< nbseries -1; i++){
-
-					Indice_util.push_back(0); //On remplit le tableau de 0, valeur par défaut de l'utilité d'une série
+					Indice_utile.push_back(0); //On remplit le tableau de 0, valeur par défaut de l'utilité d'une série
 				}
 
 
 				for(int i =0; i<bestk;i++){
 					int quotient = (Positions_min[i])/(tailletab-besth+1);
 					int reste = Positions_min[i]-quotient*(tailletab-besth+1);
-					Indice_util[quotient] =Indice_util[quotient]+ 100/(double)bestk;// On stocke en pourcentage l'interêt de la série comme sa part dans les plus proches voisins
+					Indice_utile[quotient] =Indice_utile[quotient]+ 100/(double)bestk;// On stocke en pourcentage l'interêt de la série comme sa part dans les plus proches voisins
 
 					Seq_min.push_back(Mat_totale[quotient+1][reste]); //On se place ne quotient +1 pour ne pas tomber sur la série à prévoir
 					if(reste == tailletab-besth){
@@ -350,13 +331,12 @@ vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale
 					Val_suiv.push_back( Mat_totale[quotient+1][reste+1][besth-1] );
 					}
 				}
-
-                //Pour des raisons pratiques, pour mieux le manipuler, on va transformer Indice_util en vector <double>
-                vector <double> Indice_utile;
+               
+				/*vector <double> Indice_utile;
                 for (int i=0; i< nbseries-1;i++){
 
                     Indice_utile.push_back(Indice_util[i]);
-                }
+                }*/
 
 
 
@@ -370,6 +350,7 @@ vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale
 				cout<< "La prevision pour le prochain terme de y est "<< prevision * ecart_type(tabdata[0])<<endl;
 
 
+				//Suivant la distance choisie, on va uipdater les données des tabdatap en leur ajoutant la dernière valeur préite
 				if(p==0){
                     tabdatap0[0].push_back(prevision*ecart_type(tabdata[0]));
                     Ensemble_previsionsp0.push_back(prevision * ecart_type(tabdata[0]));
@@ -398,8 +379,10 @@ vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale
 
                 Indice_utileglobal.push_back(Indice_utile);
                 cout<<" "<<endl;
+
       }//Fin du for p
 
+	  //On supprime des données updatées la première valeur pour garder des séries de même taille
       (tabdatap0[0]).erase(tabdatap0[0].begin()+0);
       (tabdatap1[0]).erase(tabdatap1[0].begin()+0);
       (tabdatap2[0]).erase(tabdatap2[0].begin()+0);
@@ -419,6 +402,8 @@ vector <vector <vector <double> > > Mat_totale; // on va stocker dans Mat_totale
 
 
       cout<<" p "<<" Series utiles "<<" Prediction "<<endl;
+
+	  //On va ici gérer l'affichage des résultats suivant nbprevisiobn et nbseries
       for (int p =0; p<3;p++){
             if(nbseries-1<= nbprevision){
                 for (int n=0;n<nbprevision;n++){
